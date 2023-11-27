78961934ef570d151b1997acc8567467df2c49f5
Ivan Ocampo <ivanocampo07@gmail.com>
2023-11-27 18:01:04 -0500

    Se incluye la funcion calcular

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+//Esta función permite que se puedan agregar numeros y simbolos a la pantalla de la calculadora.
+
 function agregar(valor){
     document.getElementById('pantalla').value += valor
 }
 
+//Esta función permite que se borren los elementos agregados a la pantalla.
+
 function borrar(){
     document.getElementById('pantalla').value = ''
+}
+
+//Esta funcion hace posible que se puedan calcular los elementos.
+
+function calcular(){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
 }
